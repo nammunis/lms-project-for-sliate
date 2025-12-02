ce5b94c5bff97b3652e248bc8603627bac6e5bba
SHEHAN <shehannsd@gmail.com>
2025-12-02 15:41:31 +0530

    Design For Rating

--- src/components/student/Rating.jsx
+++ src/components/student/Rating.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { space } from 'postcss/lib/list';
+import React, { useEffect } from 'react'
 
-const Rating = () => {
+const Rating = ({initialRating,onRate}) => {
+  const [rating, setRating] = React.useState(initialRating || 0);
+  const hadleRating = (value) => {
+    setRating(value);
+    if(onRate) onRate(value);
+  }
+
+useEffect(() => {
+  if(initialRating){
+    setRating(initialRating);
+  }},[initialRating]);
+
   return (
     <div>
-      <h1>Rating</h1>
+      {
+        Array.from({length:5},
+          (_,index)=>{
+            const starValue = index + 1;
+            return(
+              <span key={index} className={`text-xl sm:text-2xl cursor-pointer transition-colors ${starValue<= Rating? 'text-yellow-500': 'text-gray-400' }`} onClick={()=>hadleRating(starValue)}>
+                &#9733;
+              </span>
+            )
+          }
+        )
+      }
     </div>
   )
 }
