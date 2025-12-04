13c3aba0a1c60197317a5b0821a617a2171960c4
SHEHAN <shehannsd@gmail.com>
2025-12-04 08:35:40 +0530

    My Courses -Dashbord 🚀

--- src/components/educator/Footer.jsx
+++ src/components/educator/Footer.jsx
@@ -3,32 +3,28 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
+    <footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
+      <div className='flex items-center gap-4'>
+        <img className='hidden md:block w-20' src={assets.logo} alt="" />
+        <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
 
-      <Footer className="flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t">
-        <div className='flex items-center gap-4'>
-          <img className='hidden md:block w-20' src={assets.logo} alt="" />
-          <div className='hidden md:block h-7 w-px bg-gray-500/60'>
+        <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
+          Copyright © 2024 SmartStudy. All rights reserved.
+        </p>
+      </div>
 
-          </div>
-          <p className='py-4 text-center text-xs md:text-sm text-gray-500'>
-            Copyright © 2024 SmartStudy. All rights reserved.
-          </p>
-        </div>
-        <div className='flex items-center gap-3 max-md:mt-4'>
-          <a href="#">
-            <img src={assets.facebook_icon } alt="FaceBook" />
-          </a>
-          <a href="#">
-            <img src={assets.twitter_icon } alt="FaceBook" />
-          </a>
-          <a href="#">
-            <img src={assets.instagram_icon} alt="FaceBook" />
-          </a>
-
-        </div>
-
-      </Footer>
-
+      <div className='flex items-center gap-3 max-md:mt-4'>
+        <a href="#">
+          <img src={assets.facebook_icon} alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src={assets.twitter_icon} alt="Twitter" />
+        </a>
+        <a href="#">
+          <img src={assets.instagram_icon} alt="Instagram" />
+        </a>
+      </div>
+    </footer>
   )
 }
 
--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import SideBar from '../../components/educator/SideBar'
+import Footer from '../../components/educator/Footer'
 const Educator = () => {
   return (
     <div className='text-default min-h-screen bg-white'>
@@ -13,7 +14,9 @@ const Educator = () => {
         </div>
         
       </div>
+      <Footer/>
     </div>
+    
   )
 }
 
